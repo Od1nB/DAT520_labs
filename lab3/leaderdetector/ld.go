4f3f9dfe1fa527e3eda6d2c3c430a272cd46b762
Tor Haakon Andersen <toh.andersen@stud.uis.no>
2021-02-09 21:05:02 +0100

    Implement leaderdetector

--- lab3/leaderdetector/ld.go
+++ lab3/leaderdetector/ld.go
@@ -4,36 +4,57 @@ package leaderdetector
 // described at page 53 in:
 // Christian Cachin, Rachid Guerraoui, and Luís Rodrigues: "Introduction to
 // Reliable and Secure Distributed Programming" Springer, 2nd edition, 2011.
-type MonLeaderDetector struct { // TODO(student): Add needed fields
+type MonLeaderDetector struct {
+	suspected map[int]bool
+	nodeIDs   []int
+	leader    int
+	channels  []chan int
 }
 
 // NewMonLeaderDetector returns a new Monarchical Eventual Leader Detector
 // given a list of node ids.
 func NewMonLeaderDetector(nodeIDs []int) *MonLeaderDetector {
-	// TODO(student): Add needed implementation
-	m := &MonLeaderDetector{}
+	m := &MonLeaderDetector{
+		suspected: make(map[int]bool),
+		nodeIDs:   nodeIDs,
+		leader:    UnknownID,
+		channels:  make([]chan int, 0),
+	}
 	return m
 }
 
 // Leader returns the current leader. Leader will return UnknownID if all nodes
 // are suspected.
 func (m *MonLeaderDetector) Leader() int {
-	// TODO(student): Implement
-	return UnknownID
+	leader := UnknownID
+	for _, id := range m.nodeIDs {
+		if !m.suspected[id] && id > leader {
+			leader = id
+		}
+	}
+	if m.leader != leader {
+		for _, channel := range m.channels {
+			channel <- leader
+		}
+	}
+	m.leader = leader
+	return leader
 }
 
 // Suspect instructs the leader detector to consider the node with matching
 // id as suspected. If the suspect indication result in a leader change
 // the leader detector should publish this change to its subscribers.
 func (m *MonLeaderDetector) Suspect(id int) {
-	// TODO(student): Implement
+	m.suspected[id] = true
+	m.Leader()
 }
 
 // Restore instructs the leader detector to consider the node with matching
 // id as restored. If the restore indication result in a leader change
 // the leader detector should publish this change to its subscribers.
 func (m *MonLeaderDetector) Restore(id int) {
-	// TODO(student): Implement
+	delete(m.suspected, id)
+	m.Leader()
 }
 
 // Subscribe returns a buffered channel which will be used by the leader
@@ -43,8 +64,7 @@ func (m *MonLeaderDetector) Restore(id int) {
 // Note: Subscribe returns a unique channel to every subscriber;
 // it is not meant to be shared.
 func (m *MonLeaderDetector) Subscribe() <-chan int {
-	// TODO(student): Implement
-	return nil
+	channel := make(chan int, 1)
+	m.channels = append(m.channels, channel)
+	return channel // implicitly converted to read only channel
 }
-
-// TODO(student): Add other unexported functions or methods if needed.
