cd42be91f2cd1b6fee7e003a1e8d509ce3efc76a
Tor Haakon Andersen <toh.andersen@stud.uis.no>
2021-02-12 16:17:14 +0100

    Did some changes

--- lab3/singelton/one_to_rule.go
+++ lab3/singelton/one_to_rule.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	ld "da520/lab3/leaderdetector"
-	fd "dat20/lab3/failuredetector"
+	fd "dat520/lab3/failuredetector"
+	ld "dat520/lab3/leaderdetector"
 	"flag"
 	"fmt"
+	"math/rand"
+	"net"
+	"net/url"
 	"os"
 	"time"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+var myFlags arrayFlags
 
 var (
 	help = flag.Bool(
@@ -31,6 +46,11 @@ var (
 		"",
 		"Endpoint for server that will be connected to",
 	)
+	connecttoid = flag.Int(
+		"id",
+		0,
+		"Id of process to connect to first",
+	)
 	id = flag.Int(
 		"id",
 		0,
@@ -46,6 +66,7 @@ var (
 		1000,
 		"Delay used by Increasing Timout failuredetector in milliseconds",
 	)
+	nodeIPs map[int]*net.UDPAddr
 )
 
 func usage() {
@@ -54,9 +75,9 @@ func usage() {
 	flag.PrintDefaults()
 }
 
-
 func main() {
 	flag.Usage = usage
+	flag.Var(&myFlags, "connect", "Endpoints to connect to")
 	flag.Parse()
 	if *help {
 		flag.Usage()
@@ -67,10 +88,87 @@ func main() {
 	for i := 0; i < *nodes; i++ {
 		nodeIDs[i] = i
 	}
+	hbSend := make(chan fd.Heartbeat)
 	leaderdetector := ld.NewMonLeaderDetector(nodeIDs)
-	filuredetector := fd.NewEvtFailureDetector(*id, nodeIDs, leaderdetector, time.Duration(*delay)*time.Millisecond, hbSend)
+	failuredetector := fd.NewEvtFailureDetector(*id, nodeIDs, leaderdetector, time.Duration(*delay)*time.Millisecond, hbSend)
 
 	//todo: serve.UDP() function
+	endpoint, err := net.ResolveUDPAddr("udp", getEndpoint())
+	check(err)
+	nodeIPs = make(map[int]*net.UDPAddr)
+	nodeIPs[*id] = endpoint
+	conn, err := net.ListenUDP("udp", endpoint)
+	check(err)
 
-	
+	defer conn.Close()
+
+	go listen(conn, failuredetector)
+	go subscribePrinter(leaderdetector.Subscribe())
+
+	for {
+		hb := <-hbSend
+		hbByte := hbToByte(hb)
+		conn.WriteToUDP(hbByte, nodeIPs[hb.To])
+	}
+
+}
+
+func subscribePrinter(sub <-chan int) {
+	for {
+		fmt.Println(<-sub)
+	}
+}
+
+func listen(conn *net.UDPConn, failuredetector *fd.EvtFailureDetector) {
+	b := make([]byte, 512, 512)
+
+	for {
+		n, _, err := conn.ReadFromUDP(b)
+		if err != nil {
+			continue
+		}
+
+		hb := parseHeartbeat(b[:n])
+		// if _, ok := nodeIPs[hb.From]; ok {
+		// 	nodeIPs[hb.From] = a
+
+		// }
+		failuredetector.DeliverHeartbeat(hb) // todo make real heartbeat
+		// 	u.conn.WriteTo(executeCommand(c[0], c[1]), a)
+	}
+}
+
+func parseHeartbeat(b []byte) fd.Heartbeat {
+	return fd.Heartbeat{}
+}
+
+func getEndpoint() string {
+	host := *hostname
+	var err error
+	if host == "" {
+		host, err = os.Hostname()
+		if err != nil {
+			host = "localhost"
+		} else {
+			_, err = url.ParseRequestURI(host)
+			if err != nil {
+				host = "localhost"
+			}
+		}
+	}
+
+	p := *port
+	if p == 0 {
+		//random port
+		rand.Seed(time.Now().Unix())
+		p = rand.Intn(50000) + 10000
+	}
+	return host + ":" + fmt.Sprint(p)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+		os.Exit(1)
+	}
 }
