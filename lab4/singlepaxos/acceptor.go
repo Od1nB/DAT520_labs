eaea6acb0c5acf8eb54022ef9046bab4574f7dda
Odin_Bjoernebo <odinbjoernebo@gmail.com>
2021-03-01 11:12:04 +0100

    Implemented acceptor

--- lab4/singlepaxos/acceptor.go
+++ lab4/singlepaxos/acceptor.go
@@ -1,9 +1,16 @@
 package singlepaxos
 
+
 // Acceptor represents an acceptor as defined by the single-decree Paxos
 // algorithm.
-type Acceptor struct { // TODO(student): algorithm implementation
-	// Add needed fields
+type Acceptor struct { 
+	id int
+	props map[int]*Proposer //Set of proposers
+	learns map[int]*Learner //Set of learners
+	rnd Round
+	vrnd Round
+	vval Value
+
 }
 
 // NewAcceptor returns a new single-decree Paxos acceptor.
@@ -11,8 +18,16 @@ type Acceptor struct { // TODO(student): algorithm implementation
 //
 // id: The id of the node running this instance of a Paxos acceptor.
 func NewAcceptor(id int) *Acceptor {
-	// TODO(student): algorithm implementation
-	return &Acceptor{}
+	
+
+	return &Acceptor{
+		id: id,
+		props: make(map[int]*Proposer),
+		learns: make(map[int]*Learner),
+		rnd: NoRound,
+		vrnd: NoRound,
+		vval: ZeroValue	,
+	}
 }
 
 // Internal: handlePrepare processes prepare prp according to the single-decree
@@ -21,8 +36,12 @@ func NewAcceptor(id int) *Acceptor {
 // If handlePrepare returns false as output, then prm will be a zero-valued
 // struct.
 func (a *Acceptor) handlePrepare(prp Prepare) (prm Promise, output bool) {
-	// TODO(student): algorithm implementation
-	return Promise{To: -1, From: -1, Vrnd: -2, Vval: "FooBar"}, true
+
+	if  prp.Crnd > a.rnd{
+		a.rnd = prp.Crnd
+		return Promise{prp.From,a.id,a.rnd,a.vrnd,a.vval}, true
+	}
+	return Promise{},false
 }
 
 // Internal: handleAccept processes accept acc according to the single-decree
@@ -30,8 +49,13 @@ func (a *Acceptor) handlePrepare(prp Prepare) (prm Promise, output bool) {
 // corresponding learn, then output will be true and lrn contain the learn.  If
 // handleAccept returns false as output, then lrn will be a zero-valued struct.
 func (a *Acceptor) handleAccept(acc Accept) (lrn Learn, output bool) {
-	// TODO(student): algorithm implementation
-	return Learn{From: -1, Rnd: -2, Val: "FooBar"}, true
+	if acc.Rnd >= a.rnd && acc.Rnd != a.vrnd{
+		a.rnd = acc.Rnd
+		a.vrnd = acc.Rnd
+		a.vval = acc.Val
+		return Learn{a.id,a.rnd,a.vval},true
+	}
+	return Learn{}, false
 }
 
-// TODO(student): Add any other unexported methods needed.
+
